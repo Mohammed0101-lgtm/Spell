bf7b1d61656c7df3745de7794c19d3229d185c5e
Mohamed labbit <151045562+Mohammed0101-lgtm@users.noreply.github.com>
2024-08-01 01:06:22 +0100

    Update spell_ckecker.c

--- spell_ckecker.c
+++ spell_ckecker.c
@@ -19,6 +19,7 @@ typedef struct {
 typedef struct entry {
     char *key;
     int value;
+
     struct entry *next;
 } entry;
 
@@ -31,18 +32,22 @@ typedef struct map {
 unsigned long long hash(const char *str) {
     unsigned long long h = 5381;
     int c;
-    while ((c = *str++)) {
+    
+    while ((c = *str++)) 
         h = ((h << 5) + h) + c;
-    }
+    
     return h;
 }
 
 map *create_map(int capacity) {
     map *m = (map *)malloc(sizeof(map));
-    if (!m) return NULL;
+    
+    if (!m) 
+        return NULL;
 
     m->capacity = capacity;
     m->buckets = (entry**)calloc(capacity, sizeof(entry*)); // allocate memory for each bucket collectively
+    
     if (!m->buckets) {
         free(m);
         return NULL;
@@ -58,9 +63,9 @@ int get(map *m, char *key) {
 
     // iterate through the map till finding key and return it's value
     while (e != NULL) {
-        if (strcmp(key, e->key) == 0) {
+        if (strcmp(key, e->key)) 
             return e->value;
-        }
+
         e = e->next;
     }
     // if no such key is found
@@ -79,17 +84,21 @@ void put(map *m, const char *key, int value) {
             e->value = value; 
             return;
         }
+    
         e = e->next;
     }
 
     // if no existing entry with the given key 
     // is found then we should make a new entry
     entry *newEntry = (entry *)malloc(sizeof(entry));
-    if (!newEntry) return;
+   
+    if (!newEntry) 
+        return;
 
     // intialize the new entry
     newEntry->value = value;
     newEntry->key = strdup(key);
+    
     if (!newEntry->key) {
         free(newEntry);
         return;
@@ -104,18 +113,23 @@ void put(map *m, const char *key, int value) {
 void free_map(map *m) {
     for (int i = 0; i < m->capacity; i++) {
         entry *e = m->buckets[i];
+    
         while (e) {
             entry *next = e->next;
+            
             free(e->key);
             free(e);
+        
             e = next;
         }
     }
+    
     free(m->buckets);
     free(m);
 }
 
-int sub_min(int a, int b) {
+int sub_min(int a, int b) 
+{
     return a < b ? a : b;
 }
 
@@ -123,6 +137,7 @@ int sub_min(int a, int b) {
 int min_num(int a, int b, int c) {
     int x = sub_min(a, b);
     int y = sub_min(a, c);
+
     return sub_min(x, y);
 }
 
@@ -139,31 +154,33 @@ int edit_distance(char *s, char *t) {
 
     // using a table of distances to keep track of the edit distance
     int **dp = (int **)malloc((m + 1) * sizeof(int *));
-    if (!dp) return -1;
+    
+    if (!dp) 
+        return -1;
 
     // allocating memory for each row to be 'n' length
     for (int i = 0; i <= m; i++) {
         dp[i] = (int *)malloc((n + 1)*sizeof(int));
+        
         if (!dp[i]) {
             // free the previously allocated 
             // rows before failing
-            for (int j = 0; j < i; j++) { 
+            for (int j = 0; j < i; j++)  
                 free(dp[j]); 
-            }
+            
             free(dp);
+        
             return -1;
         }
     }
 
     // initialize rows
-    for (int i = 0; i <= m; i++) {
+    for (int i = 0; i <= m; i++) 
         dp[i][0] = i;
-    }
 
-    for (int j = 0; j <= n; j++) {
+    for (int j = 0; j <= n; j++) 
         dp[0][j] = j;
-    }
-
+    
     for (int i = 1; i <= m; i++) {
         for (int j = 1; j <= n; j++) {
             int sub_cost = (s[i - 1] == t[j - 1]) ? 0 : 1; // should we substitute this char
@@ -175,9 +192,9 @@ int edit_distance(char *s, char *t) {
     int result = dp[m][n];
 
     // free the memory 
-    for (int i = 0; i <= m; i++) {
+    for (int i = 0; i <= m; i++) 
         free(dp[i]);
-    }
+    
     free(dp);
 
     return result;
@@ -195,12 +212,12 @@ int binary_search_file(FILE *file, const char *target) {
         long mid = low + (high - low) / 2;
         fseek(file, mid, SEEK_SET);
 
-        if (mid != 0) {
+        if (mid != 0) 
             fgets(buffer, MAX_WORD_LEN, file);
-        }
-
+        
         long pos = ftell(file);
-        if (fgets(buffer, MAX_WORD_LEN, file) == NULL) break;
+        if (!fgets(buffer, MAX_WORD_LEN, file)) 
+            break;
 
         buffer[strcspn(buffer, "\n")] = '\0';
         int cmp = strcmp(buffer, target);
@@ -209,15 +226,18 @@ int binary_search_file(FILE *file, const char *target) {
             fseek(file, 0, SEEK_SET);
             line_num = 0;
             while (ftell(file) < pos) {
-                if (fgets(buffer, MAX_WORD_LEN, file) == NULL) break;
+                if (!fgets(buffer, MAX_WORD_LEN, file)) 
+                    break;
+                
                 line_num++;
             }
+            
             return line_num + 1;
-        } else if (cmp > 0) {
+        } 
+        else if (cmp > 0) 
             high = mid - 1;
-        } else {
+        else 
             low = ftell(file);
-        }
     }
     return -1;
 }
@@ -236,40 +256,44 @@ int binary_search_insert(FILE *file, const char *target) {
         long mid = low + (high - low) / 2;
         fseek(file, mid, SEEK_SET);
 
-        if (mid != 0) {
+        if (mid != 0) 
             fgets(buffer, MAX_WORD_LEN, file);
-        }
 
         long position = ftell(file);
-        if (!fgets(buffer, MAX_WORD_LEN, file)) break;
+        if (!fgets(buffer, MAX_WORD_LEN, file)) 
+            break;
 
         buffer[strcspn(buffer, "\n")] = '\0';
-
         int cmp = strcmp(buffer, target);
 
         if (cmp == 0) {
             fseek(file, 0, SEEK_SET);
             line_number = 0;
             while (ftell(file) < position) {
-                if (fgets(buffer, MAX_WORD_LEN, file) == NULL) break;
+                if (!fgets(buffer, MAX_WORD_LEN, file)) 
+                    break;
+                
                 line_number++;
             }
+            
             return line_number + 1;
-        } else if (cmp > 0) {
+        } 
+        else if (cmp > 0) 
             high = mid - 1;
-        } else {
+        else 
             low = ftell(file);
-        }
     }
 
     fseek(file, 0, SEEK_SET);
     line_number = 0;
+    
     while (fgets(buffer, MAX_WORD_LEN, file) != NULL) {
-        if (strcmp(buffer, target) > 0) {
+        if (strcmp(buffer, target) > 0) 
             return line_number + 1;
-        }
+        
         line_number++;
     }
+    
     return line_number + 1;
 }
 
@@ -278,38 +302,42 @@ char *get_word_by_line_number(FILE *file, int line_number) {
     int current_line = 0;
 
     fseek(file, 0, SEEK_SET);
-    while (fgets(buffer, MAX_WORD_LEN, file) != NULL) {
+    
+    while (fgets(buffer, MAX_WORD_LEN, file))
         if (++current_line == line_number) {
             buffer[strcspn(buffer, "\n")] = '\0';
             return buffer;
         }
-    }
+    
     return NULL;
 }
 
 // sorting the candidates array
 void insert_sorted(candidate *arr, int *size, candidate word) {
     int pos = 0;
-    while (pos < *size && word.dis >= arr[pos].dis) pos++;
+    while (pos < *size && word.dis >= arr[pos].dis) 
+        pos++;
 
-    for (int i = *size; i > pos; i--) arr[i] = arr[i - 1];
+    for (int i = *size; i > pos; i--) 
+        arr[i] = arr[i - 1];
 
     arr[pos] = word;
     (*size)++;
 }
 
 // prioritize candidates that have input as the prefix
-int is_prefix(char *word, char *candidate) {
+int is_prefix(char *word, char *candidate) 
+{
     return strncmp(word, candidate, strlen(word)) == 0;
 }
 
 // check if the word is in the dictionnary
 bool isCorrectSpell(char *word) {
     FILE *fp = fopen(dict, "r");
-    if (!fp) {
+   
+    if (!fp) 
         return false;
-    }
-
+    
     int index = binary_search_file(fp, word);
     fclose(fp);
 
@@ -319,57 +347,71 @@ bool isCorrectSpell(char *word) {
 // using what we implemented we look for the spelling candidates
 char **get_spelling_candidates(char *word, int *returnSize) {
     char **candidates = (char **)malloc(MAX_CANDID * sizeof(char *));
+    
     if (!candidates) {
-        fprintf(stderr, "Memory allocation failed : get_spelling_candidates()\n");
+        fprintf(stderr, 
+                    "Memory allocation failed : get_spelling_candidates()\n");
+       
         return NULL;
     }
 
     map *m = create_map(MAX_CANDID);
     if (!m) {
         free(candidates);
-        fprintf(stderr, "Memory allocation failed : get_spelling_candidates()\n");
+        fprintf(stderr, 
+                    "Memory allocation failed : get_spelling_candidates()\n");
+        
         return NULL;
     }
 
     FILE *fp = fopen(dict, "r");
+    
     if (!fp) {
         free_map(m);
         free(candidates);
-        fprintf(stderr, "File opening failed : get_spelling_candidates()\n");
+        fprintf(stderr, 
+            "File opening failed : get_spelling_candidates()\n");
+        
         return NULL;
     }
 
     int index = binary_search_insert(fp, word);
     int j = 0;
     for (int i = index - 4; i <= index + 4 && j < MAX_CANDID; i++) {
-        if (i < 1) continue;
+        if (i < 1) 
+            continue;
 
         char *candid = get_word_by_line_number(fp, i);
 
-        if (candid == NULL) continue;
-        if (candid[0] != word[0]) continue;
+        if (!candid) 
+            continue;
+        
+        if (candid[0] != word[0]) 
+            continue;
 
         int dis = edit_distance(word, candid);
 
-        if (!is_prefix(word, candid)) {
+        if (!is_prefix(word, candid)) 
             dis += 10;
-        }
 
         if (dis == -1) {
             dis = edit_distance(word, candid);
+            
             if (dis == -1) {
                 fclose(fp);
-                for (int z = 0; z < j; z++) {
-                    free(candidates[z]);
-                }
                 free_map(m);
                 free(candid);
+                for (int z = 0; z < j; z++) 
+                    free(candidates[z]);
+                
                 return NULL;
             }
         }
 
         candidates[j] = strdup(candid);
-        if (!candidates[j]) continue;
+        
+        if (!candidates[j]) 
+            continue;
 
         put(m, candidates[j], dis);
 
@@ -379,12 +421,14 @@ char **get_spelling_candidates(char *word, int *returnSize) {
     candidate *cans = (candidate *)malloc(MAX_CANDID * sizeof(candidate));
     if (!cans) {
         free_map(m);
-        for (int i = 0; i < j; i++) {
+        for (int i = 0; i < j; i++) 
             free(candidates[i]);
-        }
+        
         free(candidates);
         fclose(fp);
-        fprintf(stderr, "Memory allocation failed : get_spelling_candidates()\n");
+        fprintf(stderr, 
+                    "Memory allocation failed : get_spelling_candidates()\n");
+       
         return NULL;
     }
 
@@ -401,10 +445,9 @@ char **get_spelling_candidates(char *word, int *returnSize) {
         (*returnSize)++;
     }
 
-    for (int i = size; i < MAX_CANDID; i++) {
+    for (int i = size; i < MAX_CANDID; i++) 
         candidates[i] = NULL;
-    }
-
+    
     free_map(m);
     free(cans);
     fclose(fp);
@@ -418,6 +461,7 @@ void spell_check(char *word) {
     if (!isCorrectSpell(word)) {
         int size = 0;
         char **candidates = get_spelling_candidates(word, &size);
+    
         if (!candidates) {
             fprintf(stderr, "Failed to get spelling candidates!\n");
             return;
@@ -427,30 +471,34 @@ void spell_check(char *word) {
             printf("%s\n", candidates[i]);
             free(candidates[i]);
         }
+    
         free(candidates);
-    } else {
+    } 
+    else 
         printf("word spelling is correct!\n");
-    }
+    
 }
 
-int main() {
+int main(void) {
     char *s = (char *)malloc(NAME_MAX * sizeof(char));
+   
     if (!s) {
-        fprintf(stderr, "Memory allocation error!\n");
+        fprintf(stderr, 
+                    "Memory allocation error!\n");
         return -1;
     }
 
     printf("Enter word : ");
     if (!fgets(s, NAME_MAX, stdin)) {
         free(s);
-        fprintf(stderr, "Failed to read input!\n");
+        fprintf(stderr, 
+                    "Failed to read input!\n");
+    
         return -1;
     }
 
     s[strcspn(s, "\n")] = '\0';
-
     spell_check(s);
-
     free(s);
 
     return 0;
